b9aa36f2a19ac3f037b44d316cddd9b7a2225312
milicabacic <milicabacic99@gmail.com>
2022-07-07 13:04:04 +0200

    Inicijalizovana Spring Boot aplikacija

---
+++ src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/NpRezervacijaSaleApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.nprog.NPRezervacijaSale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpRezervacijaSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpRezervacijaSaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/NpRezervacijaSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.nprog.NPRezervacijaSale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpRezervacijaSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
