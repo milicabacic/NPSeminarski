d29e513ad7044617f1e1904877fdd87a44825ab7
milicabacic <milicabacic99@gmail.com>
2022-08-28 16:52:48 +0200

    Dodat javadoc za klasu SalaConverter

--- src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/converter/SalaConverter.java
+++ src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/converter/SalaConverter.java
@@ -4,15 +4,26 @@ import org.springframework.stereotype.Component;
 
 import rs.ac.bg.fon.nprog.NPRezervacijaSale.domain.Sala;
 import rs.ac.bg.fon.nprog.NPRezervacijaSale.dto.SalaDto;
-
+/**
+ * Klasa koja sluzi da omoguci konverzije izmedju objekata klasa Sala i SalaDto
+ * 
+ * Klasa sadrzi metodu koja dto objekat prevodi u domenski objekat, kao i metodu za prevodjenje domenskog objekta u dto objekat.
+ * 
+ * @author Milica Bacic
+ *
+ */
 @Component
 public class SalaConverter implements Converter<SalaDto, Sala>{
-
+	/**
+	 * Metoda koja objekat domenske klase Sala prevodi u dto objekat klase SalaDto.
+	 */
 	@Override
 	public SalaDto toDto(Sala e) {
 		return new SalaDto(e.getId(), e.getNaziv(), e.getKapacitet(), e.getTipSale());
 	}
-
+	/**
+	 * Metoda koja objekat dto klase SalaDto prevodi u domenski objekat klase Sala.
+	 */
 	@Override
 	public Sala toEntity(SalaDto d) {
 		Sala sala = new Sala();
