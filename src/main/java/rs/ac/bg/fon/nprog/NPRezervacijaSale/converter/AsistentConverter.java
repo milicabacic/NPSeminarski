08f21fcd5fb4ef8ca0050cb8ee04b58e3e476303
milicabacic <milicabacic99@gmail.com>
2022-08-28 16:54:01 +0200

    Dodat javadoc za klasu AsistentConverter

--- src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/converter/AsistentConverter.java
+++ src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/converter/AsistentConverter.java
@@ -4,15 +4,26 @@ import org.springframework.stereotype.Component;
 
 import rs.ac.bg.fon.nprog.NPRezervacijaSale.domain.Asistent;
 import rs.ac.bg.fon.nprog.NPRezervacijaSale.dto.AsistentDto;
-
+/**
+ * Klasa koja sluzi da omoguci konverzije izmedju objekata klasa Asistent i AsistentDto
+ * 
+ * Klasa sadrzi metodu koja dto objekat prevodi u domenski objekat, kao i metodu za prevodjenje domenskog objekta u dto objekat.
+ * 
+ * @author Milica Bacic
+ *
+ */
 @Component
 public class AsistentConverter implements Converter<AsistentDto, Asistent>{
-
+	/**
+	 * Metoda koja objekat domenske klase Asistent prevodi u dto objekat klase AsistentDto.
+	 */
 	@Override
 	public AsistentDto toDto(Asistent e) {
 		return new AsistentDto(e.getId(), e.getIme(), e.getPrezime());
 	}
-
+	/**
+	 * Metoda koja objekat dto klase AsistentDto prevodi u domenski objekat klase Asistent.
+	 */
 	@Override
 	public Asistent toEntity(AsistentDto d) {
 		
