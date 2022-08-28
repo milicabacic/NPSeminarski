e4b05032a9501198730d62c98e21d4e0a8678c63
milicabacic <milicabacic99@gmail.com>
2022-08-28 18:15:33 +0200

    Dodat javadoc za klasu AsistentController

--- src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/controller/AsistentController.java
+++ src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/controller/AsistentController.java
@@ -12,25 +12,47 @@ import org.springframework.web.bind.annotation.RestController;
 
 import rs.ac.bg.fon.nprog.NPRezervacijaSale.dto.AsistentDto;
 import rs.ac.bg.fon.nprog.NPRezervacijaSale.service.impl.AsistentServiceImpl;
-
+/**
+ * Klasa koja se odnosi na prihvatanje zahteva vezanih za domensku klasu Asistent
+ * 
+ * @author Milica Bacic
+ *
+ */
 @RestController
 @RequestMapping("/api/asistent")
 public class AsistentController {
 
+	/**
+	 * Instanca klase AsistentServiceImpl koja sluzi za pozivanje crud operacija nad domenskom klasom Asistent
+	 */
 	@Autowired
 	private AsistentServiceImpl asistentServiceImpl;
 	
-	
+	/**
+	 * 
+	 * @return Vraca listu objekata klase AsistentDto koja predstavlja sve sacuvane asistente
+	 */
 	@GetMapping("all")
 	List<AsistentDto> getAllAsistents(){
 		return this.asistentServiceImpl.getAllAsistents();
 	}
 	
+	/**
+	 * Metoda koja vraca asistenta sa zadatim id-jem
+	 * 
+	 * @param id Id asistenta koji treba da bude vracen
+	 * @return Vraca objekat klase AsistentDto koji ima zadati id
+	 */
 	@GetMapping("get/{id}")
 	AsistentDto getAsistent(@PathVariable("id") String id) {
 		return this.asistentServiceImpl.getAsistent(Long.valueOf(id));
 	}
-	
+	/**
+	 * Post metoda koja sluzi za cuvanje objekat klase AsistentDto
+	 * 
+	 * @param asistentDto Asistent koji treba da bude sacuvan
+	 * @return Vraca objekat klase AsistentDto koji predstavlja sacuvanog asistenta
+	 */
 	@PostMapping("add")
 	AsistentDto saveAsistent(@RequestBody AsistentDto asistentDto){
 		return this.asistentServiceImpl.saveAsistent(asistentDto);
