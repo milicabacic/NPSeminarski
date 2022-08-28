5e8fa64996101954251b15a5787c09a0dca453c7
milicabacic <milicabacic99@gmail.com>
2022-08-28 17:26:09 +0200

    Dodat javadoc za AsistentService

--- src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/service/AsistentService.java
+++ src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/service/AsistentService.java
@@ -3,11 +3,31 @@ package rs.ac.bg.fon.nprog.NPRezervacijaSale.service;
 import java.util.List;
 
 import rs.ac.bg.fon.nprog.NPRezervacijaSale.dto.AsistentDto;
-
+/**
+ * Interfejs koji specificira CRUD metode nad klasom AsistentDto
+ * 
+ * @author Milica Bacic
+ *
+ */
 public interface AsistentService {
-
+	/**
+	 * Metoda koja vraca objekat klase AsistentDto sa zadatim id-jem
+	 * 
+	 * @param asistentId Id asistenta koji treba da bude vracen
+	 * @return Vraca asistenta kao objekat klase AsistentDto koji ima dati id
+	 */
 	AsistentDto getAsistent(Long asistentId);
+	/**
+	 * Cuva asistenta na osnovu zadatog objekta klase AsistentDto
+	 * 
+	 * @param asistentDto Asistent koji treba da bude sacuvan
+	 * @return Vraca dto objekat koji je sacuvan
+	 */
 	AsistentDto saveAsistent(AsistentDto asistentDto);
+	/**
+	 * 
+	 * @return Vraca listu svih dto objekata koji su sacuvani
+	 */
 	List<AsistentDto> getAllAsistents();
 	
 }
