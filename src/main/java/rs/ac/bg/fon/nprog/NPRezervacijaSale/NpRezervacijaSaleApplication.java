6982394ec0c62178acf657113b057f84b58db36b
milicabacic <milicabacic99@gmail.com>
2022-08-28 18:29:20 +0200

    Dodat javadoc za klasu aplikacije

--- src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/NpRezervacijaSaleApplication.java
+++ src/main/java/rs/ac/bg/fon/nprog/NPRezervacijaSale/NpRezervacijaSaleApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+/**
+ * Klasa koja predstavlja spring boot aplikaciju i sluzi za njeno pokretanje
+ * 
+ * @author Milica Bacic
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = "rs.ac.bg.fon.nprog.NPRezervacijaSale")
 @EntityScan("rs.ac.bg.fon.nprog.NPRezervacijaSale.domain")
